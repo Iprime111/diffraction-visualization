9d05c57bd2d893dc8e6aa565fbd19d3cf8cc8cd4
Amlola <mnmnmnmn4477@gmail.com>
2025-05-03 21:53:08 +0300

    Add aperture loader from file. Add tests

---
+++ include/core/aperture.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "SFML/Graphics/Image.hpp"
+#include <string>
+
+#include "core/plane_field.hpp"
+#include "core/attributes.hpp"
+
+
+namespace diffraction {
+class Aperture : public PlaneField {
+  public:
+    Aperture(const sf::Image& image, size_t xSize, size_t ySize, double threshold);
+    
+  private:
+    sf::Image scaleImage(const sf::Image& image, std::size_t xSize, std::size_t ySize) const;
+
+    void convertToPlaneField(const sf::Image& image, double threshold);
+};
+
+// TODO aperture loader (from function)
+} // namespace diffraction
